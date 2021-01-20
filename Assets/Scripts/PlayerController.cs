990d4c38f7941f0acc680221904966d0d8e17d25
wonAdam <ehdgus5500@gmail.com>
2021-01-20 13:15:08 +0900

    Controller & Mover Abstraction

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class PlayerController : Controller
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 WASDInput = GetInput();
+        mover.ProcessInput(WASDInput);
+    }
+    private Vector2 GetInput()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        return new Vector2(h, v);
+    }
+}
---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerMover : Mover
+{
+    private CharacterController characterController;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    public override void ProcessInput(Vector2 wasd)
+    {
+        Vector3 cameraForward = Camera.main.transform.forward;
+        cameraForward = new Vector3(cameraForward.x, 0f, cameraForward.z);
+        Quaternion toCameraForward = Quaternion.FromToRotation(Vector3.forward, cameraForward);
+        Vector3 desiredDir = toCameraForward * (new Vector3(wasd.x, 0f, wasd.y));
+
+        Move(desiredDir);
+        Rotate(desiredDir);
+        ProcessAnim(cameraForward, desiredDir);
+    }
+
+    private void Move(Vector3 dir) => characterController.Move(dir * speed * Time.deltaTime);
+
+    private void Rotate(Vector3 lookDir)
+    {
+        if (lookDir.magnitude > Mathf.Epsilon)
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation, 
+                Quaternion.LookRotation(lookDir, Vector3.up), 
+                rot * Time.deltaTime);
+    }
+    private void ProcessAnim(Vector3 cameraForward, Vector3 moveDir)
+    {
+        Quaternion ToLocal = Quaternion.FromToRotation(transform.forward, cameraForward);
+        anim.SetFloat("MoveX", (ToLocal * moveDir).x);
+        anim.SetFloat("MoveZ", (ToLocal * moveDir).z);
+    }
+}
